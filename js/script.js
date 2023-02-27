00925ef6495657321542c3db15d2170cb26a4dc5
Sendangel <sendangel2016@gmail.com>
2023-02-27 19:27:44 +0400

    second commit

--- js/script.js
+++ js/script.js
@@ -11,12 +11,27 @@ const personalModieDB = {
     'privat': false,
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', 'logan'),
-      b = +prompt('На сколько оцените его?', '5'),
-      c = prompt('Один из последних просмотренных фильмов?', 'galaxy'),
-      d = +prompt('На сколько оцените его?', '6');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', 'logan'),
+          b = +prompt('На сколько оцените его?', '5');
 
-personalModieDB.movies[a] = b;
-personalModieDB.movies[c] = d;
+    if (a != null && b != null && a != '' && b != '' && a.length <50) {
+        personalModieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalModieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalModieDB.count >= 10 && personalModieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalModieDB.count > 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
 console.log(personalModieDB);
